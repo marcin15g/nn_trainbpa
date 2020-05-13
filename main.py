1d684b61155a0ebd3efdbc42220c7918978418a9
Marcin <marcin15g@gmail.com>
2020-05-13 22:44:04 +0200

    Searching for optimal parameters, testing neural network accuracy for specific parameters

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import neural_network as nn
+import numpy as np
 import plots as my_plt
 import pandas as pd
 import matplotlib.pyplot as plt
+import statistics as stat
+from matplotlib import cm
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
+from mpl_toolkits.mplot3d import Axes3D
+from random import randint
 
 #Load file
 colnames = ['Class','Alcohol','Malic acid','Ash','Alcalinity of ash','Magnesium','Total phenols','Flavanoids','Nonflavanoid phenols','Proanthocyanins','Color intensity','Hue','Dilute','Proline']
@@ -22,13 +27,100 @@ X[:] = scaler.transform(X)
 #Transform Y values into matrix
 Y = pd.get_dummies(Y)
 
-### === MAIN === ###
 
-#Train neural network
-test_ratio = 0.2 
-n_hiddenLayers = 15
-n_epochs = 2000
-showCost = True
+# === Test function definitions ===
+def test_on_parameters(n_hiddenLayers, n_epochs, showCost):        
+    # Split data into train and test sections
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=test_ratio, random_state=42)
+
+    #Train the network on train data
+    params = nn.train_nn(X_train, Y_train, n_hiddenLayers, n_epochs, adaptive_lRate, showCost=showCost)
+
+    #Test neural network on test data
+    A2, cache = nn.f_propagate(X_test, params)
+    acc = "{:.4f}".format(nn.accuracy(A2, Y_test))
+    print('Accuracy on test data: ' + acc + '%')
+
+    #Test neural network on train data
+    A2, cache = nn.f_propagate(X_train, params)
+    acc = "{:.4f}".format(nn.accuracy(A2, Y_train))
+    print('Accuracy on train data: ' + acc + '%')
+
+    # #Test neural network on all data
+    A2, cache = nn.f_propagate(X, params)
+    acc = "{:.4f}".format(nn.accuracy(A2, Y))
+    print('Accuracy on all available data: ' + acc + '%')
+
+
+def test_range__hidden_and_epochs(hidden_start, hidden_end, hidden_step, epochs_start, epochs_end, epochs_step, iterate_n):
+
+    plotX = plotY = plotZ =[]
+    for hidden in range(hidden_start, hidden_end+1,hidden_step):
+        for epoch in range(epochs_start, epochs_end+1,epochs_step):
+            acc_arr = []
+            for i in range(1,iterate_n+1):
+                X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=test_ratio, random_state=randint(1,100))        
+                params = nn.train_nn(X_train, Y_train, hidden, epoch, const_lRate, showCost=False)
+
+                pred_test, pred_cache = nn.f_propagate(X_test, params)
+                acc_test = nn.accuracy(pred_test, Y_test) 
+
+                pred_train, train_cache = nn.f_propagate(X_train, params)
+                acc_train = nn.accuracy(pred_train, Y_train)
+
+                pred_all, all_cache = nn.f_propagate(X, params)
+                acc_all = nn.accuracy(pred_all, Y)
+                print('Test: %f, Train: %f, All: %f' %(acc_test, acc_train, acc_all))
+                acc_arr.append((acc_test + acc_train + acc_all)/3)
+            plotX.append(hidden)
+            plotY.append(epoch)
+            plotZ.append(np.mean(acc_arr)) #plotZ.append(acc_test)
+            print('Hidden: %i - Epoch: %i - Average Acc: %f' %(hidden, epoch, np.mean(acc_arr)))
+
+    fig = plt.figure()
+    ax = Axes3D(fig)
+    ax.set_xlabel('Hidden layers')
+    ax.set_ylabel('Epochs')
+    ax.set_zlabel('Accuracy')
+    surf = ax.plot_trisurf(plotX, plotY, plotZ, linewidth=0.1, cmap='winter')
+    plt.savefig('./plots/plot.png')
+    plt.show()
+
+def test_params(n_hidden, n_epochs, n_iterations):
+    acc_arr = []
+    lowest_test = lowest_train = lowest_all = 100
+
+    for i in range(1, n_iterations + 1):
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=test_ratio, random_state=i)        
+        params = nn.train_nn(X_train, Y_train, n_hidden, n_epochs, adaptive_lRate, showCost=False)
+
+        pred_test, pred_cache = nn.f_propagate(X_test, params)
+        acc_test = nn.accuracy(pred_test, Y_test) 
+
+        pred_train, train_cache = nn.f_propagate(X_train, params)
+        acc_train = nn.accuracy(pred_train, Y_train)
+
+        pred_all, all_cache = nn.f_propagate(X, params)
+        acc_all = nn.accuracy(pred_all, Y) 
+
+        if(acc_test < lowest_test): lowest_test = acc_test
+        if(acc_train < lowest_train): lowest_train = acc_train
+        if(acc_all < lowest_all): lowest_all = acc_all
+
+        acc_arr.append((acc_test + acc_train + acc_all)/3)
+        print('Iteration: %i, Test: %f, Train: %f, All: %f' %(i, acc_test, acc_train, acc_all))
+
+    print('================================================')
+    print('Average accuracy for all iterations: %f' %(np.mean(acc_arr)))
+    print('Lowest accuracies >>> Test: %f, Train: %f, All: %f' %(lowest_test, lowest_train, lowest_all))
+
+
+
+
+############## === MAIN === ##############
+
+#Global params
+test_ratio = 0.2
 const_lRate = 0.01
 adaptive_lRate = {
     'InitialRate' : 0.01,
@@ -37,23 +129,31 @@ adaptive_lRate = {
     'ErrorRate' : 1.04 
 }
 
-#Split data into train and test sections
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=test_ratio)
+### TEST ON PREDEFINED PARAMETERS ###
+# n_hiddenLayers = 14
+# n_epochs = 500
+# showCost = True
 
-#Train the network on train data
-params = nn.train_nn(X_train, Y_train, n_hiddenLayers, n_epochs, adaptive_lRate, showCost=showCost)
+# test_on_parameters(n_hiddenLayers, n_epochs, showCost)
 
-#Test neural network on test data
-A2, cache = nn.f_propagate(X_test, params)
-acc = "{:.4f}".format(nn.accuracy(A2, Y_test))
-print('Accuracy on test data: ' + acc + '%')
 
-#Test neural network on train data
-A2, cache = nn.f_propagate(X_train, params)
-acc = "{:.4f}".format(nn.accuracy(A2, Y_train))
-print('Accuracy on train data: ' + acc + '%')
+### TEST RANGE FOR HIDDEN LAYERS AND EPOCHS ###
+# hidden_start = 8
+# hidden_end = 8
+# hidden_step = 11
+# epochs_start = 500
+# epochs_end = 500
+# epochs_step = 50
+# iterate_n = 1000
 
-# #Test neural network on all data
-A2, cache = nn.f_propagate(X, params)
-acc = "{:.4f}".format(nn.accuracy(A2, Y))
-print('Accuracy on all available data: ' + acc + '%')
+# test_range__hidden_and_epochs(hidden_start, hidden_end, hidden_step, epochs_start, epochs_end, epochs_step, iterate_n)
+
+
+### TEST SPECIFIC PARAMETERS FOR n INTERATIONS ###
+# n_hidden = 8
+# n_epochs = 500
+# n_iterations = 20
+
+# test_params(n_hidden, n_epochs, n_iterations)
+
+
--- tests.py
+++ tests.py
@@ -1,14 +1,10 @@
-#While testing final result, iterate 1000 times through same params, to get avarage score
+import neural_network as nn
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+from matplotlib import cm
+from mpl_toolkits.mplot3d import Axes3D
+from sklearn.model_selection import train_test_split
 
-#Try to find the optimal params for 3 train/tast ratios - 0.2 / 0.5 / 0.8
-# Find optimal n_epoch and n_hidden for the neural network
-
-#Find optimal adaptive l_rate params
-#Compare const and adaptive l_rate
-
-
-#1 Find optimal epochs and hidden_layers for 0.2 ratio - acc only
-#2 Find optimal epochs and hidden_layers for 0.5 ratio - acc only
-#3 Find optimal epochs and hidden_layers for 0.8 ratio - acc only
-#4 adaptive l_rate for 0.2 ratio - iterations for cost<0.0001 / amount of incidents (acc<60%)
-#5 compare l_rate (test range) to optimal adaptive rate - iterations for cost<0.0001
+def data_relations(file, columns):
+    return sns.pairplot(file, vars=columns, hue='Class')
